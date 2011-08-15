6dfbd69c5816d2c39d8ca6e7f5b18861ce26950a
Ender <powersoftime@hotmail.com>
2011-08-15 14:44:06 -0400

    Cantered the hen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2e6e2aae06f04d40c1235db24649833386f04c9555a209507e9776302ca0b7c52ebd46133adc495b9ec12ae394162969bb4b21909a6bc26af16ff231373e3173'
